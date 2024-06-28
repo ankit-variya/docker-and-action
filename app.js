0f66af30f5a16973816919db622b386dd0279104
ankit-variya <npdev1@unstoppablesoftwares.com>
2024-06-28 17:44:12 +0530

    add comment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
 
+//comment
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
